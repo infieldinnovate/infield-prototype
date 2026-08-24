4bcada9d469f632c4c71e9dcc6851ec1c43a0e6b
infieldinnovate <infieldinnovations@gmail.com>
2026-08-25 01:23:05 +0300

    Updated AnimatedStats.tsx

--- components/sections/AnimatedStats.tsx
+++ components/sections/AnimatedStats.tsx
@@ -63,7 +63,7 @@ interface AnimatedStatsProps {
 export function AnimatedStats({
   theme = "dark",
   scroll = true,
-  category = "solar",
+  category = "all",
 }: AnimatedStatsProps) {
   const ref = useRef<HTMLDivElement>(null);
   const [inView, setInView] = useState(false);

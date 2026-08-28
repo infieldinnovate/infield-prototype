7e107b4ddc3071dbe4f6e53822c1069db7101176
geniusdevelopers <aztechnogiz@gmail.com>
2026-08-28 11:20:08 +0300

    update

--- app/robots.ts
+++ app/robots.ts
@@ -1,12 +1,12 @@
-import type { MetadataRoute } from 'next';
-import { siteConfig } from '@/data/site.config';
+import type { MetadataRoute } from "next";
+import { siteConfig } from "@/data/site.config";
 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/api/', '/v'],
+      userAgent: "*",
+      allow: "/",
+      disallow: ["/api/", "/v", "/terms", "/privacy"],
     },
     sitemap: `${siteConfig.url}/sitemap.xml`,
   };

2130d6ac66e74eb40e1f6e622c8c2c270f46f4ad
geniusdevelopers <aztechnogiz@gmail.com>
2026-09-11 13:55:19 +0300

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -165,24 +165,6 @@ export default function RootLayout({
 
   return (
     <html lang="en" className={inter.variable}>
-      {/* <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/favicon_io/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon_io/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon_io/favicon-16x16.png"
-      /> */}
-
       <body className={styles.body}>
         <a href="#main-content" className="skip-link">
           Skip to main content
